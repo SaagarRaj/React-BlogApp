32526c0a85e2700ff0e2b71d5b625059534f065e
SaagarRaj <saagartiwari1992@gmail.com>
2024-01-25 20:03:51 -0800

    Navbar + Home post design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,15 @@
+import Navbar from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import { IoSearch } from "react-icons/io5";
+
+const Navbar = () => {
+  const user = true;
+  return (
+    <div className="flex items-center justify-between px-6 md:px-[200px] py-4 ">
+      <h1 className="text-xl font-extrabold">
+        <Link to="/">Blog Market</Link>
+      </h1>
+      <div className="flex justify-center items-center space-x-0">
+        <p>
+          <IoSearch />
+        </p>
+        <input
+          className="outline-none px-3 "
+          placeholder="Search a post"
+          type="text"
+        />
+      </div>
+      <div className="flex items-center justify-center space-x-2 md:space-x-4 ">
+        {user ? (
+          <h3>
+            <Link to="/write">Write</Link>
+          </h3>
+        ) : (
+          <h3>
+            {" "}
+            <Link to="/login">Login</Link>{" "}
+          </h3>
+        )}
+        {user ? (
+          <h3>
+            <Link to="/profile">Profile</Link>
+          </h3>
+        ) : (
+          <h3>
+            <Link to="/register">Register</Link>
+          </h3>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
